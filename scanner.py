b9682f3da13f5525371d0c5dbcc0a5c155abd7b0
brainzaps <skrypak.cookiedev@gmail.com>
2024-01-23 01:10:40 +0200

    feat: crate class for scanning

--- scanner.py
+++ scanner.py
@@ -1,8 +1,28 @@
+from token_type import TokenType
+from token import Token
+
+
 class Scanner:
 
     def __init__(self, source):
         self.source = source
         self.tokens = []
 
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
     def scan_tokens(self):
+        while not self.is_at_end():
+            # We are at the beginning of the next lexeme.
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
+    def is_at_end(self):
+        return self.current >= len(self.source)
+
+    def scan_token(self):
         pass
