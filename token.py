ad7796e14a3c66a31f13ed765c260a3f983a0d7c
brainzaps <skrypak.cookiedev@gmail.com>
2024-01-23 00:28:50 +0200

    refactor: fix shadowing

--- token.py
+++ token.py
@@ -1,6 +1,6 @@
 class Token:
-    def __init__(self, type, lexeme, literal, line):
-        self.type = type
+    def __init__(self, token_type, lexeme, literal, line):
+        self.type = token_type
         self.lexeme = lexeme
         self.literal = literal
         self.line = line
