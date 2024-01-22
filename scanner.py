a7ee40e803a28dbb58cbea6565c284f74caf9211
brainzaps <skrypak.cookiedev@gmail.com>
2024-01-23 01:31:59 +0200

    feat: recognize lexemes

--- scanner.py
+++ scanner.py
@@ -21,8 +21,39 @@ class Scanner:
         self.tokens.append(Token(TokenType.EOF, "", None, self.line))
         return self.tokens
 
+    def scan_token(self):
+        c = self.advance()
+
+        if c == '(':
+            self.add_token(TokenType.LEFT_PAREN)
+        elif c == ')':
+            self.add_token(TokenType.RIGHT_PAREN)
+        elif c == '{':
+            self.add_token(TokenType.LEFT_BRACE)
+        elif c == '}':
+            self.add_token(TokenType.RIGHT_BRACE)
+        elif c == ',':
+            self.add_token(TokenType.COMMA)
+        elif c == '.':
+            self.add_token(TokenType.DOT)
+        elif c == '-':
+            self.add_token(TokenType.MINUS)
+        elif c == '+':
+            self.add_token(TokenType.PLUS)
+        elif c == ';':
+            self.add_token(TokenType.SEMICOLON)
+        elif c == '*':
+            self.add_token(TokenType.STAR)
+
     def is_at_end(self):
         return self.current >= len(self.source)
 
-    def scan_token(self):
-        pass
+    def advance(self):
+        char = self.source[self.current]
+        self.current += 1
+
+        return char
+
+    def add_token(self, token_type, literal=None):
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(token_type, text, literal, self.line))
